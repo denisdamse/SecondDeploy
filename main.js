a499358076d37df127ef89c275a3e3103fdd11e3
Denis <denisdamse@gmail.com>
2024-11-08 14:49:11 +0200

    project done

---
+++ main.js
@@ -0,0 +1,7 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+import 'bootstrap-icons/font/bootstrap-icons.css';
+createApp(App).mount('#app')
